89dddee23c86df3728da618beeb6281d7391911f
Dan DeSousa <github@dandesousa.com>
2015-02-01 12:26:57 -0500

    fixing unit test for store

--- tests/test_store.py
+++ tests/test_store.py
@@ -24,20 +24,22 @@ class TestStore(unittest.TestCase):
 
     def test_tag_list(self):
         """tests that all tags exist and that they are returned when the regular expression is right"""
+        from lapis.models import Tag
         tag_list = ("tag1", "tag2", "tag3")
 
-        tags = list(self.__store.tags(""))
+        tags = list(self.__store.list("", cls=Tag))
         self.assertEqual(3, len(tags))
         self.assertTrue(all((tag.name in tag_list for tag in tags)))
 
-        tags = list(self.__store.tags("^tag"))
+        tags = list(self.__store.list("^tag", cls=Tag))
         self.assertEqual(3, len(tags))
         self.assertTrue(all((tag.name in tag_list for tag in tags)))
 
-        tags = list(self.__store.tags("^tag2"))
+        tags = list(self.__store.list("^tag2", cls=Tag))
         self.assertEqual(1, len(tags))
         self.assertTrue(tags[0].name == "tag2")
 
     def test_empty_tags(self):
-        tags = list(self.__store.tags("^fake_tag$"))
+        from lapis.models import Tag
+        tags = list(self.__store.list("^fake_tag$", cls=Tag))
         self.assertFalse(tags)
